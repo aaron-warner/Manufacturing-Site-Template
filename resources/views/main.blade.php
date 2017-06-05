a42e60e1cb0475580915504f3f221c4ab946b2c6
Aaron Warner <aaron.warner.web@gmail.com>
2017-06-05 12:24:53 +0100

    created directies for views and templates

---
+++ resources/views/main.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+@include('partials/_head')
+
+<body>
+
+    <!-- <a href="javascript:" id="return-to-top"><i class="fa fa-arrow-up" aria-hidden="true"></i></a> -->
+
+    @include('partials/_nav')
+
+    <div class="container-fluid content">
+
+    	@include('partials/_messages')
+
+        @yield('content')
+
+    </div>
+    <!-- end of .container -->
+
+    @include('partials/_footer')
+
+    @include('partials/_javascript')
+
+    @yield('scripts')
+
+</body>
+
+</html>
