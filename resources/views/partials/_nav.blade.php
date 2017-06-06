523f915d34786364058c3267a7364df4998f255b
Aaron Warner <aaron.warner.web@gmail.com>
2017-06-06 17:33:57 +0100

    added container-fluid to nav section

--- resources/views/partials/_nav.blade.php
+++ resources/views/partials/_nav.blade.php
@@ -1,49 +1,53 @@
-<div class="row header-container">
-	<div class="col-xs-10 col-xs-offset-1">
+<div class="container-fluid">
 
-		<div class="info-container">
-			<ul class="unstyled inline">
-				<li><i class="fa fa-map-marker" aria-hidden="true"></i>  7 Pendeford Place, Wolverhampton</li>
-				<li><i class="fa fa-clock-o" aria-hidden="true"></i>  Mon - Fri | 9:00 - 5:00</li>
-				<li><i class="fa fa-phone" aria-hidden="true"></i>  0330 088 9618</li>
-			</ul>
-		</div>
+	<div class="row header-container">
+		<div class="col-xs-10 col-xs-offset-1">
 
-		<div class="social-container">
-			<ul class="unstyled inline">
-				<li><i class="fa fa-envelope" aria-hidden="true"></i>  <a href="mailto:example@domain.com">example@domain.com</a></li>
-				<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-				<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-				<li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-				<li><a href="#"><i class="fa fa-rss" aria-hidden="true"></i></a></li>
-			</ul>
-		</div>
-
-	</div>
-</div>
-
-<div class="row menu-container">
-	<div class="col-xs-10 col-xs-offset-1">
-
-		<div class="logo-container">
-			LOGO HERE
-		</div>
-
-		<div class="nav-container">
-
-			<nav>
+			<div class="info-container">
 				<ul class="unstyled inline">
-					<li><a href="#">Home</a></li>
-					<li><a href="#">News</a></li>
-					<li><a href="#">Services</a></li>
-					<li><a href="#">Products</a></li>
-					<li><a href="#">Contact</a></li>
-					<li id="login-item"><a href="/login">Login</a></li>
-					<li id="signup-item"><a href="/register">Sign Up</a></li>
+					<li><i class="fa fa-map-marker" aria-hidden="true"></i>  7 Pendeford Place, Wolverhampton</li>
+					<li><i class="fa fa-clock-o" aria-hidden="true"></i>  Mon - Fri | 9:00 - 5:00</li>
+					<li><i class="fa fa-phone" aria-hidden="true"></i>  0330 088 9618</li>
 				</ul>
-			</nav>
+			</div>
+
+			<div class="social-container">
+				<ul class="unstyled inline">
+					<li><i class="fa fa-envelope" aria-hidden="true"></i>  <a href="mailto:example@domain.com">example@domain.com</a></li>
+					<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+					<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+					<li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+					<li><a href="#"><i class="fa fa-rss" aria-hidden="true"></i></a></li>
+				</ul>
+			</div>
 
 		</div>
-
 	</div>
+
+	<div class="row menu-container">
+		<div class="col-xs-10 col-xs-offset-1">
+
+			<div class="logo-container">
+				LOGO HERE
+			</div>
+
+			<div class="nav-container">
+
+				<nav>
+					<ul class="unstyled inline">
+						<li><a href="#">Home</a></li>
+						<li><a href="#">News</a></li>
+						<li><a href="#">Services</a></li>
+						<li><a href="#">Products</a></li>
+						<li><a href="#">Contact</a></li>
+						<li id="login-item"><a href="/login">Login</a></li>
+						<li id="signup-item"><a href="/register">Sign Up</a></li>
+					</ul>
+				</nav>
+
+			</div>
+
+		</div>
+	</div>
+
 </div>
